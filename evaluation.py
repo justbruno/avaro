1de42b20a8f3941448d5284a77c2c3344d8e87a9
Bruno <ordozgb1@aalto.fi>
2023-03-01 17:20:32 +0000

    Added more info to evaluation results

--- evaluation.py
+++ evaluation.py
@@ -9,8 +9,11 @@ from iotools import io_handler
 def evaluate(filename):
     profit = 0
     held = 0
+    nbuys,nsells = 0,0
     unmatched = 0
     portfolio_values = []
+    outstanding = []
+    revenue = 0
     with open(filename, 'r')  as f:
         for l in f:
             fields = l.strip('\n').split(' ')
@@ -23,16 +26,32 @@ def evaluate(filename):
                 #print(-price*volume*(1+FEE))
                 unmatched += 1
                 held += volume
+                nbuys += 1
+                outstanding.append((price, volume))
             elif s[0] == 'sell':
-                profit += price*volume*(1+FEE)
+                profit += price*volume*(1-FEE)
                 #print(price*volume*(1+FEE))
                 unmatched -= 1
                 held -= volume
+                nsells += 1
+
+                #print('Sold: ', volume)                
+                # Match to buy to compute revenue
+                matches = []
+                for x in outstanding:
+                    if x[1] == volume:
+                        matches.append(x)
+                #print(f'Outstanding: {outstanding}')
+                #print(f'Matches: {matches}')
+                match = matches[np.argmin([i[0] for i in matches])]
+                revenue += price*volume*(1-FEE) - match[0]*match[1]*(1+FEE)
+                outstanding = [i for i in outstanding if i[0] != match[0] or i[1] != match[1]]
+                
             #print(profit, unmatched)
             #print()
             last_price = price
             # We attach the timestamp to each observed value to improve strategy performance comparison plots
             portfolio_values.append((ts, held*last_price*(1-FEE) + profit))
 
-    return portfolio_values
+    return {'nbuys':nbuys, 'nsells':nsells, 'portfolio_values':portfolio_values, 'held':held, 'profit':profit, 'revenue':revenue}
     
